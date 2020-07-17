1a16f124f0dc5b2b669b24a6f524b79ffa60e651
gdickey273 <gdickey273@gmail.com>
2020-07-17 17:43:27 -0400

    worked a good bit more on askQuestion() function and it's not yet working. I know you're not supposed to push broken code but I don't want to lose my progress and need a break

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var timer = 120;
+var questionNumber = 1;
 var correct = 0;
 var incorrect = 0;
 var userName = "";
+
+var questionCardEl = $("#question-card");
+var answerChoiceEls = $(".answer-choice");
 
 //initialized an array of question Objects that each contain text, answerChoices[], and correctAnswer properties
 var questionArray = [
@@ -32,5 +36,56 @@ var questionArray = [
 function randomNumber(max){
   return Math.floor(Math.random()*max);
 }
+
+/*splices a random question from questionArray, updates questionCardEl to match random question (with answer options listed in random order), 
+and waits for user to answer. If users chosen answer is correct, correct counter is incremented,
+message flashes below question card to tell user their choice was correct. If the chosen answer is incorrect,
+incorrect counter is incremented, 10 is subtracted from timer, and a message flashes below questionCard to tell the user 
+their choice was incorrect.
+*/
+function askQuestion(){
+  
+  //chosing question from questionArray at random
+  var chosenQuestion = questionArray.splice(randomNumber(questionArray.length), 1)[0];
+ 
+  //set question number
+  $("#question-number").html(questionNumber);
+
+  //setting answer choices on questionCard to answer choices in question at random
+  $.each($(answerChoiceEls), function(i){
+    var r = randomNumber(chosenQuestion.answerChoices.length);
+    answerChoiceEls[i].textContent = chosenQuestion.answerChoices.splice(r,1);
+  });
+
+
+
+  answerChoiceEls.one("click", function(e){
+    if (e.target.textContent === chosenQuestion.correctAnswer){
+      correct++;
+      //CHANGE THIS TO CORRECT MESSAGE FLASH
+      console.log("correct!");
+      return;
+
+    } else{
+      incorrect++;
+      timer -= 10;
+      //CHANGE THIS TO INCORRECT MESSAGE FLASH
+      console.log("Incorrect!");
+      return;
+    }
+
+  });
+
+  
+  
+}//askQuestion()
+
+
+
+askQuestion();
+
+
+
+
 
 
